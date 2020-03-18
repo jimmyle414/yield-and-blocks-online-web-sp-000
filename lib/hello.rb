d87f25f786e0d0d774355a717e87fcd3075028c3
Jimmy Le <jimmyle414@gmail.com>
2020-03-18 13:31:31 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 
 def hello_t(array)
@@ -13,4 +14,28 @@ def hello_t(array)
   else
     puts "Hey! No block was given!"
   end
+=======
+def hello_t(array)
+  i = 0
+  
+  while i < array.length
+    yield(array[i])
+    i = i + 1
+  end
+  
+  array
+end
+
+# call your method here!
+
+def hello_t(array)
+  i = 0
+ 
+  while i < array.length
+    yield(array[i])
+    i = i + 1
+  end
+ 
+  array
+>>>>>>> fde7c4f39f3e66621d7990b6d3a220d9343c8b8d
 end
